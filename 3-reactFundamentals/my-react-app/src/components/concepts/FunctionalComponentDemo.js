daa920a533e09b3a3f4c62569cab9565761499ed
Thomas McClellan <thomasmcclellan87@gmail.com>
2018-03-15 15:03:17 -0400

    Added fat arrow section

--- 3-reactFundamentals/my-react-app/src/components/concepts/FunctionalComponentDemo.js
+++ 3-reactFundamentals/my-react-app/src/components/concepts/FunctionalComponentDemo.js
@@ -1,13 +1,77 @@
 import React from 'react';
+import {
+  Card,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+  Container,
+  Row,
+  Col
+} from 'reactstrap';
 
-const FunctionalComponentDemo = function () {
+const FunctionalComponentDemo = () => {
   return (
-    <div className='main'>
-      <div className='mainDiv'>
-        Hello React
-      </div>
-    </div>
+    <Container className='main'>
+      <Row>
+        <Col xs='12'>
+          <h1>Functional Component</h1>
+          <p>Functional components allow you to render information to the web page without having to use or change state.</p>
+          <dl>
+            <dt>Presentational</dt>
+            <dd>Often used for simply rendering a small chunk of code to the DOM</dd>
+            <dt>No 'this' keyword</dt>
+            <dd>Unlike class components, functional ones don't use 'this'</dd>
+            <dt>No state</dt>
+            <dd>These are 'dumb' components for UI.</dd>
+            <dt>Return()</dt>
+            <dd>Must return a single element.</dd>
+          </dl>
+        </Col>
+      </Row>
+      <hr/>
+      <h1>Challenge</h1>
+      <Row>
+        <Col md='6'>
+          <HelloWorldFatArrow className='logo'/>
+        </Col>
+        <Col md='6'>
+          <HelloWorld/>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
 export default FunctionalComponentDemo;
+
+const HelloWorld = function () {
+  return (
+      <div>
+          <Card>
+          <img width="100%" height="280px" src="https://i.ytimg.com/vi/BwAakF_VUV8/maxresdefault.jpg" alt="Card cap" />
+              <CardBody>
+                  <CardTitle>Regular Ole Function</CardTitle>
+                  <CardSubtitle>A JS Library</CardSubtitle>
+                  <CardText>const HelloWorld = function()</CardText>
+                  <Button>Button</Button>
+              </CardBody>
+          </Card>
+      </div>
+  )
+};
+
+//Fat Arrow Functional Component - 3 fewer lines. Common in React....
+const HelloWorldFatArrow = () =>
+  <div>
+      <Card>
+          <img width="100%" height="280px" src="https://i.ytimg.com/vi/_pfXEv9cFGE/maxresdefault.jpg" alt="Card cap" />
+          <CardBody>
+              <CardTitle>Fat Arrow</CardTitle>
+              <CardSubtitle>A JS Library</CardSubtitle>
+              <CardText>const HelloWorld = () =></CardText>
+              <Button>Button</Button>
+          </CardBody>
+      </Card>
+  </div>
