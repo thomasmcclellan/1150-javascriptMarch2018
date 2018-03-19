72cb0eb1c1c277c256247cf0e650868bd8efc4e0
Thomas McClellan <thomasmcclellan87@gmail.com>
2018-03-19 11:54:50 -0400

    3.04.5-interpolation

---
+++ 3-reactFundamentals/my-react-app/src/components/javascriptPractice/5-interpolation.js
@@ -0,0 +1,11 @@
+const username = 'Kenn';
+console.log(`${username} is the true JS Wizard!`);
+
+console.log(`Hey students, What bugs can ${username} fix for you today?`);
+
+const person = {
+  name: 'Kenn',
+  profession: 'Secret Agent'
+}
+
+console.log(`${person.name}, ${person.profession}, extraordinaire`);
