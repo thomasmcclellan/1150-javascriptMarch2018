48b63d4cc366942ba5ef1b60d80a3a4459dcc493
Thomas McClellan <thomasmcclellan87@gmail.com>
2018-03-28 11:05:42 -0400

    4.05.06-07

--- 4-expressServer/server/controllers/usercontroller.js
+++ 4-expressServer/server/controllers/usercontroller.js
@@ -2,24 +2,29 @@ var express = require('express');
 var router = express.Router();
 var sequelize = require('../db');
 var User = sequelize.import('../models/user');
-
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 /***************************************
  * Create User Endpoint: Starter
 ***************************************/
 
 router.post('/createuser', function(req, res) {
-  var username = 'The Dude'; 
-  var pass = 'therugtiedtheroomtogether';
+  var username = req.body.user.username; 
+  var pass = req.body.user.password;
 
   User.create({
     username: username,
-    passwordhash: pass
+    passwordhash: bcrypt.hashSync(pass, 10)
   })
     .then(
       function createSuccess(user) {
+
+        var token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: 60*60*24 });
+
         res.json({
           user: user,
-          message: 'created'
+          message: 'created',
+          sessionToken: token
         });
       },
       function createError(err) {
